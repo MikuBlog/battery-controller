bd4cf1a27a0a6160a269d54fe4cc327f839ad4dd
旋仔 <zixuan.wen@shopcider.com>
2023-04-28 15:21:33 +0800

    Initial Commit

---
+++ battery-controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  battery-controller
+//
+//  Created by 旋仔 on 2023/4/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ battery-controllerUITests/battery_controllerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  battery_controllerUITestsLaunchTests.swift
+//  battery-controllerUITests
+//
+//  Created by 旋仔 on 2023/4/28.
+//
+
+import XCTest
+
+final class battery_controllerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
